82081d81d9b88d9e1603380b7874dabeb2738a94
alya-dey <alexandra.deyn@gmail.com>
2022-09-08 15:56:19 +0300

    Homework_13

--- app/currency/migrations/0007_rate_source.py
+++ app/currency/migrations/0007_rate_source.py
@@ -14,6 +14,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='rate',
             name='source',
-            field=models.ForeignKey(default=None, null=True, on_delete=django.db.models.deletion.CASCADE, to='currency.source'),
+            field=models.ForeignKey(default=None, null=True, on_delete=django.db.models.deletion.CASCADE,
+                                    to='currency.source'),
         ),
     ]
